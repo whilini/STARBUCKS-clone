759c37e4cce45323eb4e31ac08ff31b12c643f36
whilini <whi0306@naver.com>
2022-04-08 20:43:54 +0900

    feat: Create Starbucks header & menu bar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Ryn!");
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", " ");
+});
